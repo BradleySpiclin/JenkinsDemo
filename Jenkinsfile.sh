da02c0ab3aecba26b7bf68d4f5e78c06b98cc64c
Larsa-mrp <bradleyspiclin@gmail.com>
2023-04-23 12:48:17 +1000

    added email step

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,6 +1,7 @@
 pipeline {
     agent any
     environment {
+        EMAIL_PATH = "bradleyspiclin@gmail.com"
         DIRECTORY_PATH = "https://github.com/BradleySpiclin/complete-ecommerce-aspnet-mvc-application.git"
         TESTING_ENVIRONMENT = "Live testing environment"
         PRODUCTION_ENVIRONMENT = "Bradley Spiclin's development environment"
@@ -13,31 +14,43 @@ pipeline {
                 echo "compile the code and generate any necessary artifacts"
             }
         }
-        stage("Test") {
+        stage("Unit and Integration Tests") {
             steps {
                 echo "Testing stage"
                 echo "Unit tests"
                 echo "Integration tests"
             }
+            post {
+                success {
+                    mail to: "${EMAIL_PATH}",
+                    subject: "Test Status Email",
+                    body: "Test Successful"
+                }
+            }
         }
-        stage("Code Quality Check") {
+        stage("Code Analysis") {
             steps {
                 echo "Check the quality of the code"
             }
         }
-        stage("Deploy") {
+        stage("Security Scan") {
             steps {
                 echo "Deply application to: ${TESTING_ENVIRONMENT}"
             }
         }
-        stage("Approval") {
+        stage("Deploy to Staging") {
             steps {
                 echo "Pausing for approval - 10 seconds"
                 sleep 10
                 echo "Approved"
             }
         }
-        stage("Deploy to Production") {
+        stage("Integration Tests on Staging") {
+            steps {
+                echo "Deploy code to production environment: ${PRODUCTION_ENVIRONMENT}"
+            }
+        }
+                stage("Integration Tests on Staging") {
             steps {
                 echo "Deploy code to production environment: ${PRODUCTION_ENVIRONMENT}"
             }
