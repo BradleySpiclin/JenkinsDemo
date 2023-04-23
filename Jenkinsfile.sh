46fb781438e337b30fa93653fd000e0a7262d100
Larsa-mrp <bradleyspiclin@gmail.com>
2023-04-23 17:23:06 +1000

    added jenkinsfile

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -2,6 +2,7 @@ pipeline {
     agent any
     environment {
         EMAIL_PATH = "bradleyspiclin@gmail.com"
+        EMAIL_BODY = "Build logs attached"
         DIRECTORY_PATH = "https://github.com/BradleySpiclin/complete-ecommerce-aspnet-mvc-application.git"
         TESTING_ENVIRONMENT = "Live testing environment"
         PRODUCTION_ENVIRONMENT = "Bradley Spiclin's development environment"
@@ -11,47 +12,72 @@ pipeline {
             steps {
                 echo "Build started"
                 echo "fetch the source code from: ${DIRECTORY_PATH}"
-                echo "compile the code and generate any necessary artifacts"
+                echo "Building using MSBuild automation tool"
             }
         }
         stage("Unit and Integration Tests") {
             steps {
                 echo "Testing stage"
-                echo "Unit tests"
-                echo "Integration tests 111"        
+                echo "Unit tests conducted using NUnit"
+                echo "Integration tests conducted using SpecFlow"        
             }
             post {
                 success {
-                    emailext(attachLog: true, to: "${EMAIL_PATH}", subject: 'hello', body: "hello")      
+                    emailext(attachLog: true, 
+                    to: "${EMAIL_PATH}", 
+                    subject: 'Unit and Integration Tests - Success', 
+                    body: "${EMAIL_BODY}")      
                 }
                 failure {
-                    emailext(attachLog: true, to: 'bradleyspiclin@gmail.com', subject: '', body: "hello")      
+                    emailext(attachLog: true, 
+                    to: "${EMAIL_PATH}", 
+                    subject: 'Unit and Integration Tests - Failed', 
+                    body: "${EMAIL_BODY}")      
                 }
             }
         }
         stage("Code Analysis") {
             steps {
-                echo "Check the quality of the code"
+                echo "Code analysis stage"
+                echo "Analyzing code using ReSharper"
             }
         }
         stage("Security Scan") {
             steps {
-                echo "Deply application to: ${TESTING_ENVIRONMENT}"
+                echo "Security scan stage"
+                echo "Scanning using Microsoft Security Code Analysis"
+            }
+            post {
+                success {
+                    emailext(attachLog: true, 
+                    to: "${EMAIL_PATH}", 
+                    subject: 'Security Scan - Success', 
+                    body: "${EMAIL_BODY}")      
+                }
+                failure {
+                    emailext(attachLog: true, 
+                    to: "${EMAIL_PATH}", 
+                    subject: 'Security Scan - Failed', 
+                    body: "${EMAIL_BODY}")      
+                }
             }
         }
         stage("Deploy to Staging") {
             steps {
-                echo "Pausing for approval - 10 seconds"
+                echo "Deploying to staging slot on Azure AppService"
             }
         }
         stage("Integration Tests on Staging") {
             steps {
                 echo "Integration tests on staging"
+                echo "API integration tests..."
+                echo "Database integration tests..."
+                echo "User interface integration tests..."
             }
         }
         stage("Deploy to Production") {
             steps {
-                echo "Deploy code to production environment: ${PRODUCTION_ENVIRONMENT}"
+                echo "Deployed to production server on Azure AppService"
             }
         }
     }
